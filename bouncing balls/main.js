4d4baa506d54772aacb180affa11e1efb5303247
Hanting <huanght@hku.hk>
2019-10-12 18:21:19 +0800

    my projects

---
+++ bouncing balls/main.js
@@ -0,0 +1,31 @@
+// setup canvas
+
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
+
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
+
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
---
+++ tetris/src/board.java
@@ -0,0 +1,10 @@
+public class board {
+    int height = 20;
+    int width = 10;
+    int[][] board = new int[width][height];
+
+    void printBoard(){
+
+    }
+
+}
